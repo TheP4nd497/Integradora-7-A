c3271aab8f41eae56a8147f693378fde0312b4a9
Jose ismael <146992395+Jismazk@users.noreply.github.com>
2025-11-23 20:01:48 -0600

    Refactor sensor setup and enhance serial output
    Updated sensor pin definitions and added water and sound sensor readings. Modified serial output format for better clarity.

--- PruebaArduino/PruebaArduino.ino
+++ PruebaArduino/PruebaArduino.ino
@@ -1,16 +1,21 @@
+
+
 #include <DHT.h>		// importa la Librerias DHT
 #include <DHT_U.h>
 
-#define MQ2_PIN A0    // sensor de gas 
-#define sonido A1     // sensor de sonido
+#define MQ2_PIN A3   // sensor de gas 
+#define sonido_mic 10     // sensor de sonido
+#define sens_mic A4
 #define LDRPIN 5      //sensor de luz
+#define agua_pin A5       // SENSOR DE AGUA 
 int dhtsens = 7;			// pin DATA de DHT22 a pin digital 2
 int TEMPERATURA;
 int HUMEDAD;
 int gas;
-//int valor_son;
-int umbralSonido = 20;  
-const int pinSonidoAnalogico =8;
+int agua;
+int valor_son;
+int sens ;
+
 
 DHT dht(dhtsens, DHT11);		// creacion del objeto, cambiar segundo parametro
 				// por DHT11 si se utiliza en lugar del DHT22
@@ -21,6 +26,9 @@ void setup(){
 
 void loop(){
 
+  //AGUA
+  agua = analogRead(agua_pin);
+
   //mq2
   gas = analogRead(MQ2_PIN);	
 
@@ -29,17 +37,38 @@ void loop(){
   TEMPERATURA = dht.readTemperature();	// obtencion de valor de temperatura
   HUMEDAD = dht.readHumidity();		// obtencion de valor de humedad
 
-  int valorSonido = analogRead(pinSonidoAnalogico);
-  if (valorSonido > umbralSonido) {
-    Serial.print("Sonido detectado: ");
-    Serial.println(valorSonido);
+  //sonido
+  valor_son = digitalRead(sonido_mic);
+  sens = analogRead(sens_mic);
+  
+
+  //luz
+ /** int32_t luz = digitalRead(LDRPIN);
+    Serial.print(" ");
+  if(luz == 1 ){
+    Serial.println("Foco Apagado ");
+  }else{
+    Serial.println("Foco Encendido ");
   }
-  Serial.print("{\"gas\":");
+  **/
+
+   Serial.print("{\"GAS\":");
   Serial.print(gas);
-  Serial.print(" ,\"HUMEDAD\":");
+  Serial.print(" ,\"HUM\":");
   Serial.print(HUMEDAD);
-  Serial.print(" ,\"TEMPERATURA\":");
+  Serial.print(" ,\"TEM\":");
   Serial.print(TEMPERATURA);
+   Serial.print(" ,\"agua\":");
+  Serial.print(agua);
+  Serial.print(" ,\"Sonido\":");
+  if (valor_son == HIGH){
+   Serial.print(1); 
+  }else{
+    Serial.print(0);
+  }
+  Serial.print(" ,\"Sonido sens\":");
+  Serial.print(sens);
   Serial.println("}");
-delay(1000);
+
+  delay(1000);
 }
